acc2f4bf1e61499465781e6857fa22e95c92eac5
Irina Avvakumova <nim20101@yandex.ru>
2018-02-16 09:20:07 +0400

    second

--- app/src/main/java/com/example/nim20/firstapp/MainActivity.java
+++ app/src/main/java/com/example/nim20/firstapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.nim20.firstapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int RC_SIGN_IN = 251;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+    @Override
+    protected void onActivityResult(int requestCod, int resultCod, Intent data){
+        super.onActivityResult(requestCod, resultCod, data);
+
+        if (requestCod==RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if(resultCod == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(this,user.toString(),Toast.LENGTH_SHORT).show();
+                } else {
+                }
+            }
+        }
     }
 }
